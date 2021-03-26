809040e4fad8ff7af111af06f7532424597937db
hanbin8269 <58972963+hanbin8269@users.noreply.github.com>
2021-03-27 00:02:51 +0900

    Update ConfigureDjango.sh

--- scripts/ConfigureDjango.sh
+++ scripts/ConfigureDjango.sh
@@ -3,6 +3,6 @@
 set -e
 
 source /opt/venv/bin/activate
-/opt/django-locallibrary-tutorial/manage.py makemigrations
+python /opt/django-locallibrary-tutorial/manage.py makemigrations
 python /opt/django-locallibrary-tutorial/manage.py migrate
 echo yes | python /opt/django-locallibrary-tutorial/manage.py collectstatic
